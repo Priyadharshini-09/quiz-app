4b51a612caf7083a6a60c0b8a50077b784d8ab23
Priyadharshini-09 <160256832+Priyadharshini-09@users.noreply.github.com>
2024-10-21 23:44:35 +0800

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,84 @@
+export const data = [
+    {
+        
+        question:" The acronyms ITES stands for _?",
+        option1:"Information Technology Entrusted Systems",
+        option2:"Information Technology Extended Services",
+        option3:"Information Technology Enabled Services",
+        option4:"Information Technology Enlighted Services",
+        ans:3,
+    },
+    {
+        question:" Which of these is not a feature of java?",
+        option1:"Object-oriented",
+        option2:"Platform-independent",
+        option3:"Compiled",
+        option4:"Interpreted language",
+        ans:3,
+
+    },
+    {
+        question:"What is the language is used write Flutterapps?",
+        option1:"Java",
+        option2:"Kotlin",
+        option3:"Swift",
+        option4:"Dart",
+        ans:4,
+
+    },{
+        question:"What is the difference between 'float' and 'double' in C?",
+        option1:"Syntax only",
+        option2:"Precision",
+        option3:"Usage",
+        option4:"No difference",
+        ans:2,
+
+    },{
+        question:"Which keyword is used to remove table from database?",
+        option1:"DROP",
+        option2:"DELETE",
+        option3:"REMOVE",
+        option4:"ALTER",
+        ans:1,
+
+    },{
+        question:"Which property is used to change textcolor of element?",
+        option1:"text-color",
+        option2:"color",
+        option3:"font-color",
+        option4:"background-color",
+        ans:2,
+
+    },{
+        question:"In operating systems, a semaphore is a _:",
+        option1:"A type of malware",
+        option2:"A low-level programming language",
+        option3:"A signal mechanism",
+        option4:"An I/O device driver",
+        ans:3,
+
+    },{
+        question:"Which of these is not a Python built-in datatype?",
+        option1:"dict",
+        option2:"array",
+        option3:"set",
+        option4:"frozenset",
+        ans:2,
+
+    },{
+        question:"Which topology does the Internet resemble?",
+        option1:"Mesh",
+        option2:"Ring",
+        option3:"Star",
+        option4:"Bus",
+        ans:1,
+
+    },{
+        question:"JSX stands for...",
+        option1:"JavaScript XML",
+        option2:"Java Syntax Extension",
+        option3:"JavaScript Syntax",
+        option4:"Java Structured XML",
+        ans:1,
+
+    },];
